2f614e0f56b0100cba12a471ff7d247f3b85a19e
onebutterflyW <ruanjiangongchengxi01@wireless-one.local>
2018-04-16 10:44:42 +0800

    Initial Commit

---
+++ newBrilliance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newBrilliance
+//
+//  Created by 软件工程系01 on 2018/4/16.
+//  Copyright © 2018年 软件工程系01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ newBrilliance/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  newBrilliance
+//
+//  Created by 软件工程系01 on 2018/4/16.
+//  Copyright © 2018年 软件工程系01. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
